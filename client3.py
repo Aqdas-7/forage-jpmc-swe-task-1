54724cde00c1a2144c0931f074c12ba1886243fe
{my <{my>
2024-05-24 22:53:20 +0530

    Fixed getDataPoint and getRatio function

--- client3.py
+++ client3.py
@@ -51,7 +51,6 @@ if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
         """ ----------- Update to get the ratio --------------- """
         prices = {}
         for quote in quotes:
